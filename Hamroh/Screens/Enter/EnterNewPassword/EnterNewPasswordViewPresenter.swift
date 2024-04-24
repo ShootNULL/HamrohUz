36ae03444263b740d2e28277a4f235a40dd83c78
Петр Тюхменев <pvtyukhmenev@gmail.com>
2024-04-24 19:02:09 +0300

    completed enter new password screen

---
+++ Hamroh/Screens/Enter/EnterNewPassword/EnterNewPasswordViewPresenter.swift
@@ -0,0 +1,19 @@
+//
+//  EnterNewPasswordViewPresenter.swift
+//  Hamroh
+//
+//  Created by Peter on 24.04.2024.
+//
+
+import Foundation
+
+protocol EnterNewPasswordViewProtocol {
+    func load() -> Void
+}
+
+class EnterNewPasswordViewPresenter: EnterNewPasswordViewProtocol {
+    
+    func load() {
+        // code
+    }
+}
